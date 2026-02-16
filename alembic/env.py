8ce18bb555bdcfb52b4f6d1358b7542f40049c0c
[alex-rav] <[aleksandr_radnovski@mail.ru]>
2026-02-16 13:04:02 +0300

    chore(alembic): setup migrations and generate initial schema

--- alembic/env.py
+++ alembic/env.py
@@ -9,6 +9,18 @@ from alembic import context
 # access to the values within the .ini file in use.
 config = context.config
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+config = context.config
+
+database_url = os.getenv("DATABASE_URL")
+
+config.set_main_option("sqlalchemy.url", database_url)
+
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -39,20 +51,6 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    import os
-    from app.database import DATABASE_URL
-    config.set_main_option("sqlalchemy.url", DATABASE_URL)
-    url = config.get_main_option("sqlalchemy.url")
-    context.configure(
-        url=url,
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-    )
-
-    with context.begin_transaction():
-        context.run_migrations()
-
 
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
